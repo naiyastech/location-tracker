f84344bd96fcd3114658594a3bec9adfa72953c8
ian-elisyan <ibacon@elisyanwealth.com>
2025-05-10 16:19:02 +0100

    Initial Commit

---
+++ LocationTest/LocationTestApp.swift
@@ -0,0 +1,32 @@
+//
+//  LocationTestApp.swift
+//  LocationTest
+//
+//  Created by Ian (US) on 10/05/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LocationTestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
